d01bdd1ec8b6a14ee51bfde9ad9b5048d22b231c
rajayogan <rajayogan27@gmail.com>
2016-04-15 01:14:22 +0530

    mocha commit

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
 var Bear = require('./index.js');
-var test = require('tape');
+//var test = require('tape');
+var assert = require('assert');
 var expect = require("chai").expect;
 
-test('should shout', function(assert){
-  var bear = new Bear();
-  var result = bear.shout();
-  assert.equal(result, 'The Bear says grr');
-  assert.end();
-})
+
+// test('should shout', function(){
+//   var bear = new Bear();
+//   var result = bear.shout();
+//   assert.equal(result, 'The Bear says grr');
+//   assert.end();
+// })
 
 describe('should return something', function(){
   it('should return a string', function(){
